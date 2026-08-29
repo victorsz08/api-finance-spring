7028046a7b995b2a117061673ece934255ab668c
victorsz08 <joaovictorsal777@gmail.com>
2026-08-29 17:07:25 -0300

    feat: implement get me user authenticated data in auth controller

--- src/main/java/com/appfinace/api/controllers/AuthController.java
+++ src/main/java/com/appfinace/api/controllers/AuthController.java
@@ -1,7 +1,11 @@
 package com.appfinace.api.controllers;
 
+import java.util.UUID;
+
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,16 +13,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.appfinace.api.dto.auth.AuthLoginRequestDto;
 import com.appfinace.api.dto.auth.AuthLoginResponseDto;
+import com.appfinace.api.dto.user.FindUserResponseDto;
+import com.appfinace.api.infra.security.UserDetailsImpl;
 import com.appfinace.api.service.AuthService;
+import com.appfinace.api.service.UserService;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
 
     private final AuthService authService;
+    private final UserService userService;
 
-    public AuthController(AuthService authService) {
+    public AuthController(AuthService authService, UserService userService) {
         this.authService = authService;
+        this.userService = userService;
     }
 
     @PostMapping("/login")
@@ -36,5 +45,28 @@ public class AuthController {
         return ResponseEntity.ok()
                 .header("Set-Cookie", cookie.toString())
                 .build();
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout() {
+        ResponseCookie cookie = ResponseCookie.from("access_token", "")
+                .httpOnly(true)
+                .secure(true)
+                .sameSite("Strict")
+                .path("/")
+                .maxAge(0)
+                .build();
+
+        return ResponseEntity.ok()
+                .header("Set-Cookie", cookie.toString())
+                .build();
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<FindUserResponseDto> getMe(@AuthenticationPrincipal UserDetailsImpl userDetatails) {
+        UUID id = userDetatails.getUser().getId();
+        FindUserResponseDto data = this.userService.findUser(id);
+
+        return ResponseEntity.ok(data);
     }
 }
