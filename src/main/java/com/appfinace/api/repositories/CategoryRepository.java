d39f5577824f4252fd276548fcb18f6c79305d65
victorsz08 <joaovictorsal777@gmail.com>
2026-09-09 20:17:50 -0300

    feat: implement method findByIdAndUserId in CategoryRepository

--- src/main/java/com/appfinace/api/repositories/CategoryRepository.java
+++ src/main/java/com/appfinace/api/repositories/CategoryRepository.java
@@ -1,6 +1,7 @@
 package com.appfinace.api.repositories;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,4 +10,6 @@ import com.appfinace.api.domain.category.Category;
 
 public interface CategoryRepository extends JpaRepository<Category, UUID> {
     public List<Category> findByUserId(UUID userId);
+
+    public Optional<Category> findByIdAndUserId(UUID id, UUID userId);
 }
