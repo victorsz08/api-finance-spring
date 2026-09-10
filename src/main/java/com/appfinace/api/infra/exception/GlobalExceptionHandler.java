88f02291ef6f1db02781d9880f0ddb797dad7a18
victorsz08 <joaovictorsal777@gmail.com>
2026-09-10 14:14:03 -0300

    feat: implement Handler Validation Exception Dtos

--- src/main/java/com/appfinace/api/infra/exception/GlobalExceptionHandler.java
+++ src/main/java/com/appfinace/api/infra/exception/GlobalExceptionHandler.java
@@ -1,7 +1,11 @@
 package com.appfinace.api.infra.exception;
 
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -35,5 +39,16 @@ public class GlobalExceptionHandler {
                 "Erro interno no servidor");
 
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(body);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ExceptionResponseDto> handleValidationException(MethodArgumentNotValidException ex) {
+        String message = ex.getBindingResult().getFieldErrors().stream()
+                .map(err -> err.getField() + ":" + err.getDefaultMessage())
+                .collect(Collectors.joining("; "));
+
+        ExceptionResponseDto body = new ExceptionResponseDto(HttpStatus.BAD_REQUEST.value(), message);
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(body);
     }
 }
