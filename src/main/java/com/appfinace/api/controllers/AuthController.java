7f01292c1d196d7baf45fdc0d691074ca80c6542
victorsz08 <joaovictorsal777@gmail.com>
2026-09-11 19:51:13 -0300

    feat: Creating swagger docs AuthController and Dto

--- src/main/java/com/appfinace/api/controllers/AuthController.java
+++ src/main/java/com/appfinace/api/controllers/AuthController.java
@@ -15,14 +15,22 @@ import org.springframework.beans.factory.annotation.Value;
 import com.appfinace.api.dto.auth.AuthLoginRequestDto;
 import com.appfinace.api.dto.auth.AuthLoginResponseDto;
 import com.appfinace.api.dto.user.UserResponseDto;
+import com.appfinace.api.infra.exception.ExceptionResponseDto;
 import com.appfinace.api.infra.security.UserDetailsImpl;
 import com.appfinace.api.service.AuthService;
 import com.appfinace.api.service.UserService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/auth")
+@Tag(name = "Autenticação", description = "Endpoints de autenticação do usuário")
 public class AuthController {
 
     private final AuthService authService;
@@ -36,6 +44,10 @@ public class AuthController {
         this.userService = userService;
     }
 
+    @Operation(summary = "Login", description = "EndPoint de login, o token de acesso é setado no cookie httpsOnly", responses = {
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "400", description = "Usuário ou senha inválidos", content = @Content(schema = @Schema(implementation = ExceptionResponseDto.class)))
+    })
     @PostMapping("/login")
     public ResponseEntity<Void> login(@Valid @RequestBody AuthLoginRequestDto data) {
         AuthLoginResponseDto response = this.authService.login(data);
@@ -53,6 +65,9 @@ public class AuthController {
                 .build();
     }
 
+    @Operation(summary = "Logout", description = "Remove o cookie de acesso do usuário", security = @SecurityRequirement(name = "cookieAuth"), responses = {
+            @ApiResponse(responseCode = "200", description = "OK")
+    })
     @PostMapping("/logout")
     public ResponseEntity<Void> logout() {
         ResponseCookie cookie = ResponseCookie.from("access_token", "")
@@ -68,6 +83,10 @@ public class AuthController {
                 .build();
     }
 
+    @Operation(summary = "Dados do usuário autenticado", security = @SecurityRequirement(name = "cookieAuth"), responses = {
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "404", description = "Usuário não localizado", content = @Content(schema = @Schema(implementation = ExceptionResponseDto.class)))
+    })
     @GetMapping("/me")
     public ResponseEntity<UserResponseDto> getMe(@AuthenticationPrincipal UserDetailsImpl userDetatails) {
         UUID id = userDetatails.getUser().getId();
--- src/main/java/com/appfinace/api/dto/auth/AuthLoginRequestDto.java
+++ src/main/java/com/appfinace/api/dto/auth/AuthLoginRequestDto.java
@@ -1,9 +1,11 @@
 package com.appfinace.api.dto.auth;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
+@Schema(name = "Dados de Login")
 public record AuthLoginRequestDto(
-        @NotBlank(message = "Email é obrigatório") @Email String email,
-        @NotBlank(message = "Senha é obrigatória") String password) {
+                @Schema(description = "Email do usuário") @NotBlank(message = "Email é obrigatório") @Email String email,
+                @Schema(description = "Senha do usuário") @NotBlank(message = "Senha é obrigatória") String password) {
 }
