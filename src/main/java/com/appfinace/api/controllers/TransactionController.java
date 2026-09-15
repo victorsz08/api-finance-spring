55202de1d349b38fc1a9dc6beecdae2da032bbc6
victorsz08 <joaovictorsal777@gmail.com>
2026-09-14 21:11:14 -0300

    docs: add swagger operation and annotations to TransactionController

--- src/main/java/com/appfinace/api/controllers/TransactionController.java
+++ src/main/java/com/appfinace/api/controllers/TransactionController.java
@@ -16,13 +16,22 @@ import org.springframework.web.bind.annotation.RestController;
 import com.appfinace.api.dto.transaction.MonthlySummaryResponseDto;
 import com.appfinace.api.dto.transaction.TransactionRequestDto;
 import com.appfinace.api.dto.transaction.TransactionResponseDto;
+import com.appfinace.api.infra.exception.ExceptionResponseDto;
 import com.appfinace.api.infra.security.UserDetailsImpl;
 import com.appfinace.api.service.TransactionService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/transactions")
+@Tag(name = "Transações", description = "Endpoits de gerenciamento de transações")
 public class TransactionController {
 
     private final TransactionService transactionService;
@@ -31,6 +40,12 @@ public class TransactionController {
         this.transactionService = transactionService;
     }
 
+    @Operation(summary = "Criar transação", security = @SecurityRequirement(name = "cookieAuth"), responses = {
+            @ApiResponse(responseCode = "201", description = "OK"),
+            @ApiResponse(responseCode = "404", description = "Categoria não localizada", content = @Content(schema = @Schema(implementation = ExceptionResponseDto.class))),
+            @ApiResponse(responseCode = "404", description = "Usuário não localizado", content = @Content(schema = @Schema(implementation = ExceptionResponseDto.class))),
+            @ApiResponse(responseCode = "400", description = "Dados incorretos", content = @Content(schema = @Schema(implementation = ExceptionResponseDto.class)))
+    })
     @PostMapping
     public ResponseEntity<Void> create(
             @AuthenticationPrincipal UserDetailsImpl user,
@@ -41,9 +56,13 @@ public class TransactionController {
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    @Operation(summary = "Listar transações filtradas", security = @SecurityRequirement(name = "cookieAuth"), responses = {
+            @ApiResponse(responseCode = "200", description = "OK")
+    })
     @GetMapping
-    public ResponseEntity<List<TransactionResponseDto>> listByMonth(@RequestParam(required = false) Integer month,
-            @RequestParam(required = false) Integer year,
+    public ResponseEntity<List<TransactionResponseDto>> listByMonth(
+            @Parameter(description = "Filtro parcial por mês", example = "9") @RequestParam Integer month,
+            @Parameter(description = "Filtro parcial por ano", example = "2026") @RequestParam Integer year,
             @AuthenticationPrincipal UserDetailsImpl user) {
         UUID userId = user.getUser().getId();
         List<TransactionResponseDto> transactions = this.transactionService.listByMonth(month, year, userId);
@@ -51,9 +70,13 @@ public class TransactionController {
         return ResponseEntity.ok(transactions);
     }
 
+    @Operation(summary = "Buscar resumo mensal", security = @SecurityRequirement(name = "cookieAuth"), responses = {
+            @ApiResponse(responseCode = "200", description = "OK")
+    })
     @GetMapping("/summary")
-    public ResponseEntity<MonthlySummaryResponseDto> getMonthlySummary(@RequestParam(required = true) Integer month,
-            @RequestParam(required = true) Integer year,
+    public ResponseEntity<MonthlySummaryResponseDto> getMonthlySummary(
+            @Parameter(description = "Filtro por mês", example = "9") @RequestParam(required = true) Integer month,
+            @Parameter(description = "Filtro por ano", example = "2026") @RequestParam(required = true) Integer year,
             @AuthenticationPrincipal UserDetailsImpl user) {
         UUID userId = user.getUser().getId();
         MonthlySummaryResponseDto summary = this.transactionService.getMonthlySummary(month, year, userId);
